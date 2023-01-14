78162a209a58cf5b4efa260dd0bdd441bd55602b
Username <87919916+AP0tato@users.noreply.github.com>
2023-01-14 14:23:50 -0500

    Added more player factors

--- Main/Main.py
+++ Main/Main.py
@@ -4,23 +4,25 @@ import os
 import sys
 
 class Player:
-    xVelocity = 0.0
-    yVelocity = 0.0
-    stress = 0  # Max 100
+    xVelocity = 0
+    yVelocity = 0
+    stress = 50  # Max 100
     health = 80 # Max 100, factors such as eating habits and physical activity
+    rest = 100
+    satisfaction = 80
 
 p = Player()
 
 def on_press(key):
     try:
         if(key.char=='w'):
-            p.yVelocity = 1.0
+            p.yVelocity = 1
         elif(key.char=='s'):
-            p.yVelocity = -1.0
+            p.yVelocity = -1
         elif(key.char=='a'):
-            p.xVelocity = -1.0
+            p.xVelocity = -1
         elif(key.char=='d'):
-            p.xVelocity = 1.0
+            p.xVelocity = 1
     except AttributeError:
         pass
 
@@ -28,8 +30,8 @@ def on_release(key):
     if key==keyboard.Key.esc:
         sys.exit(0)
     elif( key.char=='w' or key.char=='s' or key.char=='a' or key.char=='d' ):
-        p.xVelocity = 0.0
-        p.yVelocity = 0.0
+        p.xVelocity = 0
+        p.yVelocity = 0
 
 def Main():
     listener = keyboard.Listener(
