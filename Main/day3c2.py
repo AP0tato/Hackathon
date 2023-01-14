684c675695e31c7bfb37cd159ee5a0b5631aa297
lillypop <122584962+lillypop1101@users.noreply.github.com>
2023-01-14 17:21:04 -0500

    Create day3c2.py
    just check to see if stats are what you'd think is ok

---
+++ Main/day3c2.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import Globals as g
+
+
+window = tk.Tk()
+
+myVar = tk.StringVar(window)
+myVar.set("Select")
+
+
+window.title("Day 3.2")
+window.geometry("800x500")
+title = window.title()
+
+
+day  = tk.Label(window, text = "Day 3", font = "times 20", height = 0, width = 0)
+day.place(x=0,y=0)
+
+
+message = tk.Label(window, text = "I've been sitting all day, maybe I should move around...", font = "times 10", height=5, bg = "white")
+message.place(x=250,y=340)
+
+walk = tk.Button(window,text='Walk for 20 min', command=lambda:walk())
+walk.place(x=260, y=430)
+
+gym = tk.Button(window,text='Go to a Gym', command=lambda:gym())
+gym.place(x=460, y=430)
+
+
+
+def walk():
+    #adds, but less
+    window.destroy()
+    g.p.health += 2
+    import day4
+
+def gym():
+    #adds, but more
+    window.destroy()
+    g.p.health+= 4
+    import day4
+
+window.mainloop()
