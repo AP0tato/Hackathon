8065f79f3e7dd0b70d853bbdc53688c2cbfff2ac
lillypop <122584962+lillypop1101@users.noreply.github.com>
2023-01-14 14:37:17 -0500

    start screen updated

--- Main/gameStart.py
+++ Main/gameStart.py
@@ -6,11 +6,18 @@ window.geometry('800x500')
 myVar = tk.StringVar(window)
 myVar.set("Select")
 
-window.title("Game Screen")
+window.title("Intro")
 window.geometry("800x500")
 title = window.title()
 
-welcomeLabel = tk.Label(window, text = "Welcome\nThis is the start! ", height = 5, width = 20)
+welcomeLabel = tk.Label(window, text = "Culminating Time (n.)\n\n A stressful time where students forget to balance out studying and health\n\nAnd I'm no different. But I won't this time.", height = 100, width = 500)
 welcomeLabel.pack()
+
+contBtn = tk.Button(window,text='Continue', command=lambda:show_window())
+contBtn.place(x=360, y=340)
+
+def show_window():
+    window.destroy()
+    import day1-1
 
 window.mainloop()
