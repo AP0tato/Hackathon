6ca56375579fdf80179ac04470a6c1f7fe5f2d05
lillypop <122584962+lillypop1101@users.noreply.github.com>
2023-01-14 15:02:29 -0500

    Create day1c2.py
    choice 2, sleep, no graphics/factor

---
+++ Main/day1c2.py
@@ -0,0 +1,39 @@
+from pynput import keyboard
+import tkinter as tk
+import _thread as th
+import sys
+
+window = tk.Tk()
+
+myVar = tk.StringVar(window)
+myVar.set("Select")
+
+
+window.title("Day 1.2")
+window.geometry("800x500")
+title = window.title()
+
+
+day  = tk.Label(window, text = "Day 1", font = "times 20", height = 0, width = 0)
+day.place(x=0,y=0)
+
+
+message = tk.Label(window, text = "It's pretty late now. I don't really feel tired but I also don't have much to do...", font = "times 10", height=5, bg = "white")
+message.place(x=245,y=340)
+
+sleep = tk.Button(window,text='Go to Sleep', command=lambda:healthy())
+sleep.place(x=260, y=430)
+
+stayUp = tk.Button(window,text='Stay Up', command=lambda:unhealthy())
+stayUp.place(x=460, y=430)
+
+
+def unhealthy():
+    window.destroy()
+    import day2c1
+
+def healthy():
+    window.destroy()
+    import day2c1
+
+window.mainloop()
