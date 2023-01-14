26056954949893cceddea18e6133b3cd0cd160c7
Username <87919916+AP0tato@users.noreply.github.com>
2023-01-13 20:55:32 -0500

    Some stuff

--- Main/Main.py
+++ Main/Main.py
@@ -2,11 +2,23 @@ import keyboard as key
 import tkinter as tk
 import _thread as th
 
+xVelocity = 0.0
+yVelocity = 0.0
+
 def CheckInput():
     while True:
         try:
-            if(key.is_pressed()):
-                print(1)
+            if(key.is_pressed('w')):
+                yVelocity = 1.0
+            elif(key.is_pressed('s')):
+                yVelocity = -1.0
+            elif(key.is_pressed('a')):
+                xVelocity = -1.0
+            elif(key.is_pressed('d')):
+                xVelocity = 1.0
+            else:
+                xVelocity = 0.0
+                yVelocity = 0.0
         except:
             continue
 
