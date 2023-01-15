5ad1b2b635d2b967dbcdbfe935c521b2222bf76a
lillypop <122584962+lillypop1101@users.noreply.github.com>
2023-01-14 19:48:17 -0500

    Create day7.py

---
+++ Main/day7.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import Globals as g
+
+
+window = tk.Tk()
+
+myVar = tk.StringVar(window)
+myVar.set("Select")
+
+
+window.title("Day 7")
+
+w = 800 
+h = 500 
+
+ws = window.winfo_screenwidth()
+hs = window.winfo_screenheight() 
+
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+title = window.title()
+
+
+day  = tk.Label(window, text = "Day 7", font = "times 20", height = 0, width = 0)
+day.place(x=0,y=0)
+
+
+message = tk.Label(window, text = "I slept in on my day off. There isn't a lot of time left in the day.\nMy old classmates asked me out to a small outing\nI think I deserve a break...", font = "times 10", height=5, bg = "white")
+message.place(x=250,y=340)
+
+stat = tk.Label(window, text = "Stress:"+str(g.p.stress) + "\nHealth:"+str(g.p.health) + "\nRest:"+str(g.p.rest) + "\nHappiness:"+str(g.p.readiness), font = "times 10", height=5)
+stat.place(x=650,y=0)
+
+friends= tk.Button(window,text='Go With Classmates', command=lambda:friends())
+friends.place(x=450, y=430)
+
+review = tk.Button(window,text='Stay Home to Study', command=lambda:review())
+review.place(x=240, y=430)
+
+
+
+def review():
+    window.destroy()
+    g.p.happiness-=9
+    g.p.rest-=8
+    g.p.readiness+=9
+    g.p.stress+=6
+    g.p.health -= 8
+    import day8c1
+
+def friends():
+    window.destroy()
+    g.p.happiness +=9
+    g.p.stress+=3
+    g.p.readiness-=7
+    g.p.rest-=8
+
+    import day8c1
+
+window.mainloop()
