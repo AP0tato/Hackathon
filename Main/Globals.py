b9967f838c7bde4c441f79dff67f5093e5a4da91
Username <87919916+AP0tato@users.noreply.github.com>
2023-01-14 19:52:21 -0500

    Changes

--- Main/Globals.py
+++ Main/Globals.py
@@ -1,7 +1,7 @@
 class Player:
     stress = 85        # Max 100
     health = 80        # Max 100, factors such as eating habits and physical activity
-    rest = 90          # Max 100
+    rest = 85          # Max 100
     happiness = 80  # Max 100
     readiness = 70
 
--- Main/day1c1.py
+++ Main/day1c1.py
@@ -25,8 +25,14 @@ day.place(x=0,y=0)
 message = tk.Label(window, text = "Today is the first day, I've decided to start small. Good in, good out!\nSo what should I eat?", font = "times 10", height=5, bg = "white")
 message.place(x=245,y=340)
 
-stat = tk.Label(window, text = "Stress:"+str(g.p.stress) + "\nHealth:"+str(g.p.health) + "\nRest:"+str(g.p.rest) + "\nHappiness:"+str(g.p.readiness), font = "times 10", height=5)
-stat.place(x=650,y=0)
+stat = tk.Label(window,font = "times 16", height=5)
+
+text = tk.Text(window)
+text.insert('1.0', "Stress:"+str(g.p.stress) + "\nHealth:"+str(g.p.health) + "\nRest:"+str(g.p.rest) + "\nHappiness:"+str(g.p.readiness))
+
+
+stat.place(x=690,y=0)
+
 
 fast = tk.Button(window,text='Fast Food', command=lambda:unhealthy())
 fast.place(x=260, y=430)
